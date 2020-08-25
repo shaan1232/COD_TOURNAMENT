f45edda3b922f5b053dda7f24a3eec91132e8703
shaan1232 <70190194+shaan1232@users.noreply.github.com>
2020-08-25 06:14:47 -0400

    Update Tournament.java

--- CodTourny/Tournament.java
+++ CodTourny/Tournament.java
@@ -3,37 +3,14 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.*;
 
-/*
-We are gonna need each players current state for tiered, since if a player loses they are out of the tourny and the
-winner goes on, so we need a boolean playercheck to go alongside each player name.
-
-We can have a round status. start of every round every player is true in this variable, meaning they hadn't played yet.
-we can use it to randomize all our matches.
-
-Swap playerstatus and playerroundstatus to arraylists *********************
-
-https://stackoverflow.com/questions/26471421/round-robin-algorithm-implementation-java HOW TO DO ROUND ROBIN
-*/
 public class Tournament{
 
   private int playerCount;
   public String[] names;
-  //private boolean[] playerStatus;
-
   private ArrayList<Boolean> playerStatus;
   private ArrayList<Boolean> playerRoundStatus;
 
-  //private boolean[] playerRoundStatus;
-
   public Tournament(int gameType){
-
-    /*
-    Variable definitions
-    int playerCount: # of players participating in Tournament
-    int tournament: tournament type. 1 for Tiered 2 for R-R
-    */
-
-    // input prompt: # of players
 
     Scanner input = new Scanner(System.in);
     System.out.println("Enter Number of players (must be of 2^n for tiered tourny):");
@@ -55,33 +32,21 @@ public class Tournament{
       names[i] = input.next();
       playerStatus.add(true);
       playerRoundStatus.add(true);
-
     }
-
     if (gameType == 2 && (playerCount % 2 == 1)){
       playerCount++;
     }
-
-
-
-
-
   }
+  
   public int getPlayerCount(){
     return playerCount;
   }
 
   public String getName(int i){
     return names[i];
-
   }
 
-  //public void setName(int i){
-  //  names[i] = "empty";
-  //}
-
   public void newTierRefresh(){
-
     for (int i = 0; i < playerCount; i++) {
       playerRoundStatus.set(i,true);
     }
@@ -92,18 +57,14 @@ public class Tournament{
   }
 
   public void setPlayerRoundStatus(int p1, int p2){
-    //playerRoundStatus[p1] = false;
     playerRoundStatus.set(p1,false);
-    //playerRoundStatus[p2] = false;
     playerRoundStatus.set(p2,false);
   }
 
 
   public void setPlayerStatus(int winner, int liberal){
     playerStatus.set(winner,true);
-    //playerStatus[winner] = true;
     playerStatus.set(liberal,false);
-    //playerStatus[liberal] = false;
   }
 
   public boolean getPlayerRoundStatus(int i){
@@ -112,13 +73,11 @@ public class Tournament{
 
   public boolean getPlayerStatus(int i){
     return playerStatus.get(i);
-    //return playerStatus[i];
   }
 
   public int getRemainingPlayers(){
     int numPlayersRemaining = 0;
     for (int i = 0; i < playerCount; i++){
-    //  if (playerStatus[i] == true){
       if (playerStatus.get(i) == true){
         numPlayersRemaining++;
       }
