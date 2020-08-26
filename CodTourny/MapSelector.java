7b82805df349309b74b4fd607632908414a9bfb3
shaan1232 <70190194+shaan1232@users.noreply.github.com>
2020-08-25 21:28:19 -0400

    Added new map
    Verdansk_Stadium

--- CodTourny/MapSelector.java
+++ CodTourny/MapSelector.java
@@ -7,24 +7,29 @@ public class MapSelector{
   private ArrayList<String> chosenMaps;
 
   public MapSelector(int nMaps){
-    
-    String[] toWord = {"Aisle_9","Atrium","Bazaar","Cargo","Docks","Gulag","Hill","King","LiveStock","Pine","Rust","Shipment","SpeedBall","Stack","Trench"};
+    String[] toWord = {"Aisle_9","Atrium","Bazaar","Cargo","Docks","Gulag","Hill","King","LiveStock","Pine","Rust","Shipment","SpeedBall","Stack","Trench","Verdansk_Stadium"};
+
+
     chosenMaps = new ArrayList<String>();
+
+
     Scanner input = new Scanner(System.in);
     int playerChoice;
-    
-    printMapList(); 
+
+
+    printMapList();
     for (int i = 0; i < nMaps; i++){
       System.out.println("Enter map digit for choice #"+(i+1));
       playerChoice = input.nextInt();
+//      System.out.println("Added map '"+printMapList(playerChoice)+" to tournament.'\n");
       chosenMaps.add(toWord[playerChoice]);
     }
   }
-  
-  private void printMapList(){
-    String[] toWord = {"Aisle_9","Atrium","Bazaar","Cargo","Docks","Gulag","Hill","King","LiveStock","Pine","Rust","Shipment","SpeedBall","Stack","Trench"};
 
-    for (int i = 0; i < 15; i++){
+  private void printMapList(){
+    String[] toWord = {"Aisle_9","Atrium","Bazaar","Cargo","Docks","Gulag","Hill","King","LiveStock","Pine","Rust","Shipment","SpeedBall","Stack","Trench","Verdansk_Stadium"};
+
+    for (int i = 0; i < 16; i++){
       System.out.println("CODE:"+i+" MAP: "+toWord[i]);
     }
   }
@@ -32,4 +37,5 @@ public class MapSelector{
   public String getCurrentMap(int i){
     return chosenMaps.get(i);
   }
+
 }
